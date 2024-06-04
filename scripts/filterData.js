7a644c4e663db595c07e8ac400a3b13623aa7cbe
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-06-05 00:08:39 +0530

    Update filterData.js

--- scripts/filterData.js
+++ scripts/filterData.js
@@ -8,13 +8,21 @@ const filterData = (data, sortInputs) => {
     return false;
   });
 
+  function shuffleArray() {
+    for (let i = filteredData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [filteredData[i], filteredData[j]] = [filteredData[j], filteredData[i]];
+    }
+  }
+  shuffleArray();
+
   if (sortByMktCap)
-    filteredData.sort((a, b) => Number(a.market_cap) - Number(b.market_cap));
+    filteredData.sort((a, b) => Number(b.market_cap) - Number(a.market_cap));
   if (sortByPercentage)
     filteredData.sort(
       (a, b) =>
-        Number(a.price_change_percentage_24h) -
-        Number(b.price_change_percentage_24h)
+        Number(b.price_change_percentage_24h) -
+        Number(a.price_change_percentage_24h)
     );
   return filteredData;
 };
